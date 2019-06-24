e29a971200808db8a781a93163c9251feb81bcb6
Unknown <blondin.boris@gmail.com>
2019-06-24 15:57:22 +0100

    merge packages node script

---
+++ merge-packages.js
@@ -0,0 +1,32 @@
+var merge = require('merge-package-json');
+const fs = require('fs');
+const dl = require('directory-list');
+
+var packages = require(__dirname + '/package-base.json');
+
+const themeDir = __dirname+'/Themes/';
+const moduleDir = __dirname+'/Modules/';
+
+dl.list(themeDir, true, function(dirs) {
+	for(var index in dirs){
+		if (fs.existsSync(themeDir+dirs[index]+'/package.json')) {
+			var package = require(themeDir+dirs[index]+'/package.json');
+			packages = merge(packages, package);
+		}
+	}
+});
+
+dl.list(moduleDir, true, function(dirs){
+	for(var index in dirs){
+		if (fs.existsSync(moduleDir+dirs[index]+'/package.json')) {
+			var package = require(moduleDir+dirs[index]+'/package.json');
+			packages = merge(packages, package);
+		}
+	}
+});
+
+
+packages = JSON.parse(packages);
+let data = JSON.stringify(packages, null, 4);
+
+fs.writeFileSync('package.json', data);
