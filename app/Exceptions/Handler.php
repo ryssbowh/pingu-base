182140cfb6b6d8fbe8e30bea95c56959b88752d3
Unknown <blondin.boris@gmail.com>
2019-06-30 13:19:29 +0100

    tweaks

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        \Theme::setDefault();
         return parent::render($request, $exception);
     }
 }
